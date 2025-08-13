a3c09c04ab608041404fc8aef6b8cde99e62ef2d
aidansunbury <aidansunbury@gmail.com>
2025-08-12 21:47:05 -0700

    remove test

--- packages/trpc-ui/src/index.ts
+++ packages/trpc-ui/src/index.ts
@@ -1,4 +1,3 @@
 export { renderTrpcPanel } from "./render";
 export type { TRPCPanelMeta } from "./meta";
-export { parseRouterWithOptions } from "./parse/parseRouter";
 export { parseTRPCRouter } from "./parseV2/parse";
